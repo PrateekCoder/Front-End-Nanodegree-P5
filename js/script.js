711d7577cdacab8043986f84b99d23674898a7eb
Prateek Gaurav <prateekgaurav23@gmail.com>
2016-03-29 16:44:26 +0530

    Add Wikipedia Links API

--- js/script.js
+++ js/script.js
@@ -17,15 +17,15 @@ function loadData() {
 
     $greeting.text('So, you want to live at ' + address + '?');
 
-
     // load streetview
-
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
 
-    // load nytimes
 
+
+    // load nytimes
+    // obviously, replace all the "X"s with your own API key
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=600c376119330dee8534be72daeb9fcc:8:74825624';
     $.getJSON(nytimesUrl, function(data){
 
@@ -45,9 +45,29 @@ function loadData() {
     });
 
 
-    // load wikipedia data
 
-    // YOUR CODE GOES HERE!
+    // load wikipedia data
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        jsonp: "callback",
+        success: function( response ) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
 
     return false;
 };
